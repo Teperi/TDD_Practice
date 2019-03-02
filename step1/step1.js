97d355a3dfc74bb0cd17c2a7e24ecf372f172abe
Teperi Lee <teperi31@gmail.com>
2019-03-02 20:34:37 +0900

    init: 첫 환경구성 및 jest tutorial
    npm jest 설치
    jest 공식 문서 튜토리얼 진행
    step1 테스트 예정

---
+++ step1/step1.js
@@ -0,0 +1,43 @@
+/*
+1. 질문의 결과 - 계좌 생성
+let account = new Account()
+
+if (account === null) {
+    console.err("계좌 생성 실패")
+} else {
+    console.log("성공")
+}
+*/
+
+/* 
+1. 응답 - 에러 잡기
+class Account {}
+
+let account = new Account()
+
+if (account === undefined) {
+    console.error("계좌 생성 실패")
+} else {
+    console.log(account)
+    console.log("성공")
+}
+*/
+
+/*
+1. 정제 - 코드 정제 및 Todo list 정리
+*/
+class Account {}
+
+let account = new Account()
+
+try {
+
+} catch (error) {
+
+}
+
+if (account === undefined) {
+    console.error("계좌 생성 실패")
+} else {
+    console.log("성공")
+}
